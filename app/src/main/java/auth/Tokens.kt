bb186fed5be0d6b9044515e8c5c2ace1694383f1
binrynoise <binarynoise@mail.de>
2019-11-17 17:01:20 +0100

    Fix UnsatisfiedLinkError

--- app/src/main/java/auth/Tokens.kt
+++ app/src/main/java/auth/Tokens.kt
@@ -1,12 +1,5 @@
 package de.binarynoise.appdate
 
 object Tokens {
-	
-	val googleCred: String by lazy { getGoogleCredNative() }
-	
-	init {
-		System.loadLibrary("native-lib")
-	}
-	
-	private external fun getGoogleCredNative(): String
+	val googleCred: String = "" //TODO: add your credentials
 }
